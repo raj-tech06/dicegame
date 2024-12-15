ef798a20ff254203822f94e1904bcdb554074dee
Abhishek Sahu <rjdeveloper07@gmail.com>
2024-12-15 12:22:18 +0530

    code

---
+++ dicegame.js
@@ -0,0 +1,117 @@
+// let dice=document.querySelector("#dice")
+
+
+
+
+let roll=()=>{
+   let rol=Math.round(Math.random()*6)
+   if(rol==1){
+    // rol(img1.style.display="block");
+    img1.style.display="block"
+    img2.style.display="none"
+    img3.style.display="none";
+    img4.style.display="none";
+    img5.style.display="none";
+    img6.style.display="none";
+
+
+   }
+   else if(rol==2){
+    
+   
+    img1.style.display="none"
+     img2.style.display="block"
+    img3.style.display="none";
+    img4.style.display="none";
+    img5.style.display="none";
+    img6.style.display="none";
+   
+   }
+   else if(rol==3){
+ 
+      img1.style.display="none"
+      img2.style.display="none"
+     img3.style.display="block";
+     img4.style.display="none";
+     img5.style.display="none";
+     img6.style.display="none";
+    
+   }
+   else if(rol==4){
+   img1.style.display="none"
+   img2.style.display="none"
+  img3.style.display="none";
+  img4.style.display="block";
+  img5.style.display="none";
+  img6.style.display="none";
+   }
+   else if(rol==5){
+    img1.style.display="none"
+    img2.style.display="none"
+   img3.style.display="none";
+   img4.style.display="none";
+   img5.style.display=" block";
+   img6.style.display="none";
+   
+   }
+   else if(rol==6){
+   img1.style.display="none"
+   img2.style.display="none"
+  img3.style.display="none";
+  img4.style.display="none";
+  img5.style.display="none";
+  img6.style.display="block";
+   }
+  
+   
+}
+// =============================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let rol2=Math.round(Math.random()*6)
+// let roll2=()=>{
+ 
+//     if(rol2==1){
+//      // rol(img1.style.display="block");
+//      img1.style.display="block"
+//      // rol(img2.style.display="none");
+//     }
+//     else if(rol2==2){
+//      // rol(img2.style.display="block");
+//      img2.style.display="block"
+    
+//     }
+//     else if(rol2==3){
+//      // rol(img3.style.display="block");
+//      img3.style.display="block"
+     
+//     }
+//     else if(rol2==4){
+//      // rol(img4.style.display="block");
+//     img4.style.display="block"
+//     }
+//     else if(rol2==5){
+//      // rol(img5.style.display="block");
+//      img5.style.display="block"
+    
+//     }
+//     else if(rol2==6){
+//      // rol(img6.style.display="block");
+//   img6.style.display="block"
+//     }
+   
+    
+//  }
